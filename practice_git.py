91a7c62e7ce97049b1aaa7b0c28fe6aba6f4d210
khanapureakshay <84321154+khanapureakshay@users.noreply.github.com>
2024-05-20 12:48:00 +0530

    Simple changes

--- practice_git.py
+++ practice_git.py
@@ -5,3 +5,12 @@ print('Coool...........Akshay')
 
 def add(a,b,c,d):
     return a+b+c+d
+
+def sub(a,b):
+    return a-b
+
+def check_diff():
+    return print('Okay.. here is diff..')
+
+def stash():
+    return print('Lets not make any commits for now...')
